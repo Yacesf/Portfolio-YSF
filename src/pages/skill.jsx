956469545dd2307b31d558b4b7ba77d84ca62a80
Yace Sf <yacine.sf@outlook.fr>
2024-12-24 00:14:18 +0100

    creation d'un infinite marquee pour le bandeau skill

--- src/pages/skill.jsx
+++ src/pages/skill.jsx
@@ -1,5 +1,59 @@
+import ReactLogo from "../assets/Logo-de-competence/Logo-React.png";
+import TailwindCSSLogo from "../assets/Logo-de-competence/tailwind.svg"
+import SASSLogo from "../assets/Logo-de-competence/Logo-Sass.png"
+import ReduxLogo from "../assets/Logo-de-competence/Logo-redux.png"
+import NextJsLogo from "../assets/Logo-de-competence/nextjs-removebg-preview.png"
+import ViteLogo from "../assets/Logo-de-competence/vitejs2-removebg-preview.png"
+
+
 function Skill() {
-  return <div>Bonjour</div>;
+  const skillLogo = [
+    { name: "React", src: ReactLogo },
+    { name: "TailwindCSS", src: TailwindCSSLogo },
+    { name: "SASS", src: SASSLogo },
+    { name: "Redux", src: ReduxLogo },
+    { name: "Next.js", src: NextJsLogo },
+    { name: "Vite", src: ViteLogo },
+  ];
+
+  return (
+    <div className="mt-40 h-36 overflow-hidden rounded-b-[80px] flex bg-vert justify-center">
+    <div className=" flex items-center gap-10 animate-marquee w-max-none">
+      {skillLogo.map((skill) => (
+        <img
+          className="h-20 invert brightness-0 "
+          alt={`Logo de ${skill.name}`}
+          src={skill.src}
+          key={skill.name}
+        />
+      ))}
+      {skillLogo.map((skill) => (
+          <img
+            className="h-20 invert brightness-0"
+            alt={`Logo de ${skill.name}`}
+            src={skill.src}
+            key={`${skill.name}-duplicate`}
+          />
+        ))}
+        {skillLogo.map((skill) => (
+          <img
+            className="h-20 invert brightness-0"
+            alt={`Logo de ${skill.name}`}
+            src={skill.src}
+            key={`${skill.name}-duplicate-2`}
+          />
+        ))}
+        {skillLogo.map((skill) => (
+          <img
+            className="h-20 invert brightness-0"
+            alt={`Logo de ${skill.name}`}
+            src={skill.src}
+            key={`${skill.name}-duplicate-3`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Skill;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
       colors: {
-        'vert': '#617f67',
-        'creme': '#fff6e7',
+        vert: "#617f67",
+        creme: "#fff6e7",
       },
       fontFamily: {
-        inter: ['Inter'],
-        poppins: ['Poppins'],
+        inter: ["Inter"],
+        poppins: ["Poppins"],
       },
     },
   },
   plugins: [],
-}
-
+};
